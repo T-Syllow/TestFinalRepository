050cfe50e392ac38e88d18137393bfc3fc616a08
Tommy Syllow <tommysyllow@gmail.com>
2022-05-09 15:37:43 +0200

    Username und Password auf private gesetzt und Getter & Setter eingefügt.

--- ZZ - TestFinalRepository/src/testFinalRepository/objects/User.java
+++ ZZ - TestFinalRepository/src/testFinalRepository/objects/User.java
@@ -2,12 +2,30 @@ package testFinalRepository.objects;
 
 public class User {
 
-	public String username;
-	public String password;
+	private String username;
+	private String password;
 	
 	public User (String username, String password) {
 		this.username = username;
 		this.password = password;
 	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
 	
 }
