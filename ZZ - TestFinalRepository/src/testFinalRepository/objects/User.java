acee7435397ad0fa893ae7d66c2b442a7a9f2ad9
Tommy Syllow <tommysyllow@gmail.com>
2022-06-20 21:44:00 +0200

    User.java Felder entfernt. sollte jetzt bereit sein zum mergen

--- ZZ - TestFinalRepository/src/testFinalRepository/objects/User.java
+++ ZZ - TestFinalRepository/src/testFinalRepository/objects/User.java
@@ -2,8 +2,7 @@ package testFinalRepository.objects;
 
 public class User {
 
-	private String username;
-	private String password;
+	
 	
 	public User (String username, String password) {
 		this.username = username;
