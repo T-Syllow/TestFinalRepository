0d91b853e404574d2de6b0d6076f44ce06e26de7
Tommy Syllow <tommysyllow@gmail.com>
2022-06-20 21:34:42 +0200

    User.java isEingabekorrekt Methode erstellt.

--- ZZ - TestFinalRepository/src/testFinalRepository/objects/User.java
+++ ZZ - TestFinalRepository/src/testFinalRepository/objects/User.java
@@ -2,12 +2,38 @@ package testFinalRepository.objects;
 
 public class User {
 
-	public String username;
-	public String password;
+	public static String username;
+	public static String password;
+	
 	
 	public User (String username, String password) {
 		this.username = username;
 		this.password = password;
 	}
+	
+	public static boolean isEingabekorrekt(String name, String pwd) {
+		if(name.equals(username) && pwd.equals(password)) {
+			return true;
+		}
+		return false;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
 	
 }
