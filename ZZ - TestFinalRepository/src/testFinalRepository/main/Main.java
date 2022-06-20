11ec6b88ffec643545777a5b25837649477d90c4
Tommy Syllow <tommysyllow@gmail.com>
2022-06-20 21:59:51 +0200

    Main.java new user

--- ZZ - TestFinalRepository/src/testFinalRepository/main/Main.java
+++ ZZ - TestFinalRepository/src/testFinalRepository/main/Main.java
@@ -7,6 +7,8 @@ public class Main {
 	public static void main(String[] args) {
 		User user = new User("Mark", "123qwerty");
 		System.out.println(user.toString());
+		User user2 = new User("Dennis", "awsdkkk22");
+		System.out.println(user2.toString());
 		
 
 	}
